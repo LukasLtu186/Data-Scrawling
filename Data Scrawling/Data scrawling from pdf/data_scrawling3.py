fe3afc473ffefb0004610735add99b156bf766eb
LukasLtu186 <98620961+LukasLtu186@users.noreply.github.com>
2023-12-13 16:13:13 +0200

    Delete Data Scrawling directory

--- Data Scrawling/Data scrawling from pdf/data_scrawling3.py
+++
@@ -1,9 +0,0 @@
-import tabula
-
-tables = tabula.read_pdf("ast_sci_data_tables_sample.pdf", pages="all" )
-# print(tables)
-df = tables[1] # data frame
-print(type(df))
-print(df)
-print()
-print(df[df.Car == "Spirit of America"])
